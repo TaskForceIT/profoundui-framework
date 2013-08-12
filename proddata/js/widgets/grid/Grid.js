TaskForceIT/profoundui-framework

commit cdffc19a9fec5b4ebb0644e087003343a9eca5c5

Fix grid sort icon being lost when user resizes columns.

proddata/js/widgets/grid/Grid.js
@@ -1436,6 +1436,26 @@ pui.Grid = function() {
         me.cells[0][i].innerHTML = '<div style="' + paddingCSS + alignCSS + '">' + me.columnHeadings[i] + '</div>';
         centerHeadingVertically(me.cells[0][i]);
       }      
+      // This method runs when the user resizes columns, and the sort icon becomes orphaned.
+      var placeIcon = false;
+      if (me.designMode == false && me.sortable == true) {      
+        if (clientSortColumnId == me.cells[0][i].columnId) {
+          placeIcon = true;
+        }
+        else if (me.tableDiv.columnSortResponseField != null && me.initialSortColumn == me.cells[0][i].columnId) {
+          placeIcon = true;
+        }
+        if (placeIcon) {
+          if (me.sortIcon.parentNode != null) {
+            me.sortIcon.parentNode.removeChild(me.sortIcon);
+          }
+          var destination = me.cells[0][i];
+          if (destination.firstChild != null && destination.firstChild.tagName == "DIV") {
+            destination = destination.firstChild;
+          }
+          destination.appendChild(me.sortIcon);
+        }
+      }
     }
   }
   

commit c767bf296f0adfc2dbc38a717ff6afa38cc801de

Fix bug introduced in commit cdffc19a9fec5b4ebb0644e087003343a9eca5c5 that would cause error when Grid "setHeadings()" method runs before "makeSortable()" method.

proddata/js/widgets/grid/Grid.js
@@ -1462,7 +1462,7 @@ pui.Grid = function() {
       }      
       // This method runs when the user resizes columns, and the sort icon becomes orphaned.
       var placeIcon = false;
-      if (me.designMode == false && me.sortable == true) {      
+      if (me.designMode == false && me.sortable == true && me.sortIcon != null) {      
         if (clientSortColumnId == me.cells[0][i].columnId) {
           placeIcon = true;
         }