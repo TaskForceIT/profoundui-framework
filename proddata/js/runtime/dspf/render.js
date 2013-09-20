TaskForceIT/profoundui-framework

commit aff0481cf0a1cf14c307b012f38be8368f165c2e

When multiple grids are controlled by one record format, allow paging links for each grid to function independently.

proddata/js/runtime/dspf/render.js
@@ -2155,8 +2155,13 @@ pui.attachResponse = function(dom) {
       }
     }
     
+    // Little fix here: Do not pickup doms with matching shortcut key if 
+    // we are dealing with a grid paging bar item. 
+    
+    // This allows paging controls for grids in same control format to 
+    // function independently of one another.
     var doms = [];
-    if (dom.shortcutKey != null) {
+    if (dom.shortcutKey != null && !dom.parentPagingBar) {
       pui.keyName = dom.shortcutKey;
       for (formatName in pui.keyMap) {
         var keyMapDomArray = pui.keyMap[formatName][pui.keyName];

commit 051f1093bc2c43d06cb209657bf19912d4e29461

Fix bug introduced in commit aff0481cf0a1cf14c307b012f38be8368f165c2e which caused AID byte to not be sent to the server when grid paging links are clicked.

proddata/js/runtime/dspf/render.js
@@ -2219,6 +2219,12 @@ pui.attachResponse = function(dom) {
       }
     }
     
+    if (dom.shortcutKey != null) {
+    
+      pui.keyName = dom.shortcutKey;
+    
+    }
+    
     // Little fix here: Do not pickup doms with matching shortcut key if 
     // we are dealing with a grid paging bar item. 
     
@@ -2226,7 +2232,6 @@ pui.attachResponse = function(dom) {
     // function independently of one another.
     var doms = [];
     if (dom.shortcutKey != null && !dom.parentPagingBar) {
-      pui.keyName = dom.shortcutKey;
       for (formatName in pui.keyMap) {
         var keyMapDomArray = pui.keyMap[formatName][pui.keyName];
         if (keyMapDomArray != null) {