TaskForceIT/profoundui-framework

commit 9560624e030132de84547dd3c16d0bd49af71c43

Corrected failure of date field and combo box widget when "input type" property is set to a type which is unsupported by the browser, and standardized method of accomplishing this in textbox widget.

proddata/js/widgets/combo_box.js
@@ -529,7 +529,7 @@ pui.widgets.add({
     },
 
     "input type": function(parms) {
-      if (!parms.design && parms.dom.comboBoxWidget != null) parms.dom.comboBoxWidget.getBox().type = parms.value;
+      if (!parms.design && parms.dom.comboBoxWidget != null) parms.dom.comboBoxWidget.getBox().setAttribute("type", parms.value);
     }
 
   },

proddata/js/widgets/date_field.js
@@ -529,7 +529,7 @@ pui.widgets.add({
     },
     
     "input type": function(parms) {
-      if (!parms.design) parms.dom.type = parms.value;
+      if (!parms.design) parms.dom.setAttribute("type", parms.value);
     }
 
   

proddata/js/widgets/textbox.js
@@ -51,10 +51,7 @@ pui.widgets.add({
     
     "input type": function(parms) {
       if (!parms.design) {
-        try {  // browser may not support a particular input type
-          parms.dom.type = parms.value;
-        }
-        catch(e) { }
+        parms.dom.setAttribute("type", parms.value);
       }
     }
   

commit 0ec7db29cdc54901b321419744a58dc711920358

Fix problem introduced in commit 9560624e030132de84547dd3c16d0bd49af71c43 that caused IE6/7/8 to fail with 'This command is not supported.' when setting one of the new HTML5 input types.  Since these old versions of IE do not support HTML5, it will now just catch/ignore the error.

proddata/js/widgets/combo_box.js
@@ -529,7 +529,11 @@ pui.widgets.add({
     },
 
     "input type": function(parms) {
-      if (!parms.design && parms.dom.comboBoxWidget != null) parms.dom.comboBoxWidget.getBox().setAttribute("type", parms.value);
+      try {
+        if (!parms.design && parms.dom.comboBoxWidget != null) 
+          parms.dom.comboBoxWidget.getBox().setAttribute("type", parms.value);
+      }
+      catch(e) { }
     }
 
   },

proddata/js/widgets/date_field.js
@@ -529,7 +529,9 @@ pui.widgets.add({
     },
     
     "input type": function(parms) {
-      if (!parms.design) parms.dom.setAttribute("type", parms.value);
+      try {
+        if (!parms.design) parms.dom.setAttribute("type", parms.value);
+      } catch(e) { }
     }
 
   

proddata/js/widgets/textbox.js
@@ -51,7 +51,9 @@ pui.widgets.add({
     
     "input type": function(parms) {
       if (!parms.design) {
-        parms.dom.setAttribute("type", parms.value);
+\       try { 
+          parms.dom.setAttribute("type", parms.value);
+        } catch(e) { }
       }
     }
   