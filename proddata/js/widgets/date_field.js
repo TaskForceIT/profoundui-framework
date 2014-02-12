TaskForceIT/profoundui-framework

commit 078383d4a92de6c8d45ee457afaf6c45e33d1029

Adjust how the popup calendar advances 1 month at a time

proddata/js/widgets/date_field.js
@@ -57,10 +57,19 @@ function show_calendar(dateField, str_datetime, format) {
 	var dt_datetime = (str_datetime == null || str_datetime == "" ?  new Date() : str2dt(str_datetime));
 	if(dt_datetime==null) dt_datetime = new Date();
 
+  var curMonth = dt_datetime.getMonth();
 	var dt_prev_month = new Date(dt_datetime);
-	dt_prev_month.setMonth(dt_datetime.getMonth()-1);
+	dt_prev_month.setMonth(curMonth - 1);
+	// check if the date overflowed to the following month, and if so go back (in JavaScript, substracting a month from March 31st gives you March 3rd, February being a short month)
+	while (dt_prev_month.getMonth() == curMonth) {
+	  dt_prev_month.setDate(dt_prev_month.getDate() - 1);
+	}
 	var dt_next_month = new Date(dt_datetime);
-	dt_next_month.setMonth(dt_datetime.getMonth()+1);
+	dt_next_month.setMonth(curMonth + 1);
+	// check if the date overflowed to the following month, and if so go back (in JavaScript, adding a month to January 31st gives you a date in the beginning of March)
+	while (dt_next_month.getMonth() != curMonth + 1) {
+	  dt_next_month.setDate(dt_next_month.getDate() - 1);
+	}
 	var dt_prev_year = new Date(dt_datetime);
 	dt_prev_year.setFullYear(dt_datetime.getFullYear()-1);
 	var dt_next_year = new Date(dt_datetime);

commit 88bb3cce5478bedb16bd89694cb3d16480dfc49d

Correct infinite loop when advancing date picker 1 month from November, introduced in commit 078383d4a92de6c8d45ee457afaf6c45e33d1029

proddata/js/widgets/date_field.js
@@ -67,7 +67,9 @@ function show_calendar(dateField, str_datetime, format) {
 	var dt_next_month = new Date(dt_datetime);
 	dt_next_month.setMonth(curMonth + 1);
 	// check if the date overflowed to the following month, and if so go back (in JavaScript, adding a month to January 31st gives you a date in the beginning of March)
-	while (dt_next_month.getMonth() != curMonth + 1) {
+	var next = curMonth + 1;
+	if (next == 12) next = 0;
+	while (dt_next_month.getMonth() != next) {
 	  dt_next_month.setDate(dt_next_month.getDate() - 1);
 	}
 	var dt_prev_year = new Date(dt_datetime);