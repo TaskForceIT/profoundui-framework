TaskForceIT/profoundui-framework

commit 76550135a6646c7e5d82bb39d1945ac5b884fe9d

Render grid column move pointers to document body to prevent clipping in some situations.

proddata/js/widgets/grid/Grid.js
@@ -3415,11 +3415,37 @@ pui.Grid = function() {
                   columnPointer.src = pui.normalizeURL("/profoundui/proddata/images/grids/column-pointer.gif");
                   columnPointer.style.position = "absolute";
                   columnPointer.style.zIndex = me.moveColumnZIndex + 1;
-                  line.parentNode.appendChild(columnPointer);
+                  if (me.designMode) toolbar.designer.container.appendChild(columnPointer); 
+                  else document.body.appendChild(columnPointer);
                 }
+                var prt = line.parentNode;
+                var top = parseInt(line.style.top) - 10;
+                var left = parseInt(line.style.left) - 4;
+                var offset = {x:0, y:0};
+                if (context == "dspf" && prt.getAttribute("container") == "true") {
+                
+                  offset = pui.layout.getContainerOffset(prt);
+                  
+                }
+                else if (prt.isPUIWindow) {
+                
+                  offset.x = prt.offsetLeft;
+                  offset.y = prt.offsetTop;
+                  
+                }
+                var ctrOffset;
+                if (!me.designMode) {
+                
+                  ctrOffset = pui.getOffset(pui.runtimeContainer);
+                  offset.x += ctrOffset[0];
+                  offset.y += ctrOffset[1];                         
+                
+                }
+                top += offset.y;
+                left += offset.x;                
                 columnPointer.style.display = "";
-                columnPointer.style.top = (parseInt(line.style.top) - 10) + "px";
-                columnPointer.style.left = (parseInt(line.style.left) - 4) + "px";
+                columnPointer.style.top = top + "px";
+                columnPointer.style.left = left + "px";
                 matchedCol = i;
                 columnPointer.matchedCol = matchedCol;
                 break;

commit 94e78607e1db32e786da49e3079ea5978804463a

Correct grid column move indicator positioning issue introduced with 76550135a6646c7e5d82bb39d1945ac5b884fe9d.

proddata/js/widgets/grid/Grid.js
@@ -3433,14 +3433,6 @@ pui.Grid = function() {
                   offset.y = prt.offsetTop;
                   
                 }
-                var ctrOffset;
-                if (!me.designMode) {
-                
-                  ctrOffset = pui.getOffset(pui.runtimeContainer);
-                  offset.x += ctrOffset[0];
-                  offset.y += ctrOffset[1];                         
-                
-                }
                 top += offset.y;
                 left += offset.x;                
                 columnPointer.style.display = "";