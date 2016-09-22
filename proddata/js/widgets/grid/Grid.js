TaskForceIT/profoundui-framework

commit b78a51a0385eec871bbaf1cf8e21c23b8aee93a8

Fixed grid removeColumn when "movable columns" is true. The parameter specifies the original column, before moving. Issue 2875.

proddata/js/widgets/grid/Grid.js
@@ -5225,22 +5225,40 @@ pui.Grid = function() {
     }
     positionIcons();
     me.setScrollBar();
-  }
+  };
   
-  this["removeColumn"] = function(colIndex) {
+  /**
+   * Remove a column.
+   * @param {Number} columnId  The original columnId, as positioned in design-time.
+   * @returns {undefined}
+   */
+  this["removeColumn"] = function(columnId) {
     var lastCol = me.vLines.length - 2;
     if (lastCol < 1) {
-      pui.alert("You cannot remove the last column.");
+      pui.alert(pui["getLanguageText"]("runtimeMsg","cannot rmv last col"));
       return;
     }
-    me.moveColumn(colIndex, me.cells[0].length);  // move column to end
+    // Find the current position of the columnId. (User may have moved it.)
+    var col = -1;
+    for(var i=0; i < me.cells[0].length; i++){
+      if( me.cells[0][i].columnId == columnId){
+         col = me.cells[0][i].col;
+         break;
+      }
+    }
+    if(col < 0){
+      pui.alert(pui["getLanguageText"]("runtimeMsg","cannot find col"));
+      return;
+    }
+    
+    me.moveColumn(col, me.cells[0].length);  // move column to end
     me.removeLastColumn();
     me.doExpandToLayout();
     me.selectMe();
-  }
+  };
   
   this.removeLastColumn = function() {
-    n = me.vLines.length;
+    var n = me.vLines.length;
     if (n <= 0) return;
     n = n - 1;
     me.vLines[n].parentNode.removeChild(me.vLines[n]);
@@ -5256,7 +5274,7 @@ pui.Grid = function() {
     setLineWidths();
     positionIcons();
     me.setScrollBar();
-  }
+  };
   
   this.moveColumn = function(from, to) {
     for (var row = 0; row < me.cells.length; row++) {
@@ -5283,7 +5301,7 @@ pui.Grid = function() {
       function movePropertyParts(propName) {
         var value = itm.properties[propName];
         if (value == null || value == "" || pui.isBound(value) || pui.isTranslated(value)) return;
-        arr = value.split(",");
+        var arr = value.split(",");
         if (arr.length == 1 && propName != "column headings" && propName != "column widths") {
           // One value is applicable to all columns
           return;
@@ -5377,7 +5395,7 @@ pui.Grid = function() {
 
       me["alignColumnTotals"]();
     }
-  }
+  };
   
   this.addRow = function() {
     var n = me.hLines.length;

proddata/js/runtime/messages.js
@@ -151,6 +151,8 @@ pui["runtimeMsg"]["en_US"]["data src not specfd x"]   = "Data source not specifi
 pui["runtimeMsg"]["en_US"]["name fld not specfd x"]   = "Name field not specified for &1...";
 pui["runtimeMsg"]["en_US"]["val fld not specfd x"]    = "Value field not specified for &1...";
 pui["runtimeMsg"]["en_US"]["failed to load x"]        = "Failed to load &1.";
+pui["runtimeMsg"]["en_US"]["cannot rmv last col"]     = "You cannot remove the last column.";
+pui["runtimeMsg"]["en_US"]["cannot find col"]         = "Cannot find the specified columnId.";
 // Atrium only.
 pui["runtimeMsg"]["en_US"]["num sessions exceeded"]   = "Number of allowed sessions exceeded.";
 pui["runtimeMsg"]["en_US"]["unable to load portal"]   = "Unable to load portal settings or navigation items.";

commit bc45d7e1aed7f29ef70d8f7b8dcdf89b06d6347a

Fixed removeColumn in Designer broken when right-clicking column heading to remove; broken since commit b78a51a. Issue 3027.

proddata/js/widgets/grid/Grid.js
@@ -5421,11 +5421,15 @@ pui.Grid = function() {
       pui.alert(pui["getLanguageText"]("runtimeMsg","cannot rmv last col"));
       return;
     }
-    // Find the current position of the columnId. (User may have moved it.)
-    var col = getCurrentColumnFromId(columnId);
-    if(col < 0){
-      pui.alert(pui["getLanguageText"]("runtimeMsg","cannot find col"));
-      return;
+    if (!me.designMode){
+      // Find the current position of the columnId. (User may have moved it.)
+      var col = getCurrentColumnFromId(columnId);
+      if(col < 0){
+        pui.alert(pui["getLanguageText"]("runtimeMsg","cannot find col"));
+        return;
+      }
+    }else{
+      var col = columnId; //Designer must use cell.col not cell.columnId.
     }
     
     me.moveColumn(col, me.cells[0].length);  // move column to end