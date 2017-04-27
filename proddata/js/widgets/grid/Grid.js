TaskForceIT/profoundui-framework

commit 849235ec24ff8db183cd8cc5d9ee47d5a5b52b41

Paging links and PgUp/PgDn decide target rowNum instead of setting scrollTop then calculating rowNum based on scrollTop. #3152.

proddata/js/widgets/grid/Grid.js
@@ -1383,7 +1383,11 @@ pui.Grid = function() {
       if (me.scrollbarObj != null && me.scrollbarObj.type == "sliding") {
         me.scrollbarObj.totalRows = dataRecords.length;
       }
-      if (me.tableDiv.style.visibility != "hidden" && me.scrollbarObj != null) me.scrollbarObj.draw();
+      if (me.tableDiv.style.visibility != "hidden" && me.scrollbarObj != null){
+        me.scrollbarObj.draw();
+        // Sliding scrollbar must know when grid is done loading so that a flag can be cleared after queued functions finish.
+        if (typeof me.scrollbarObj.gridRequestFinished == "function") me.scrollbarObj.gridRequestFinished();
+      }
       if (me.pagingBar != null) {
         me.pagingBar.pageNumber = parseInt((me.recNum + numRows * 2 - 2) / numRows) + me.initialPageNumber - 1;
         me.pagingBar.draw();
@@ -1471,7 +1475,11 @@ pui.Grid = function() {
           }
         }
       }
-      if (me.tableDiv.style.visibility != "hidden" && me.scrollbarObj != null) me.scrollbarObj.draw();
+      if (me.tableDiv.style.visibility != "hidden" && me.scrollbarObj != null){
+        me.scrollbarObj.draw();
+        // Sliding scrollbar must know when grid is done loading so that a flag can be cleared after queued functions finish.
+        if (typeof me.scrollbarObj.gridRequestFinished == "function") me.scrollbarObj.gridRequestFinished();
+      }
 
       // Update the pagingbar after each response.
       if (me.pagingBar != null) {

proddata/js/widgets/grid/SlidingScrollBar.js
@@ -50,6 +50,7 @@ pui.SlidingScrollBar = function() {
   var rowNumHideRequest = 0;
   var firstRequest = true;
   var mouseWheelEnabled = false;
+  var finishTimeout = null;   // for a timeout in gridRequestFinished().
 
   var me = this;
 
@@ -198,13 +199,13 @@ pui.SlidingScrollBar = function() {
             return;
           }
           //touchHandle.style.opacity = 1;
-          var touch = e.touches[0]
+          var touch = e.touches[0];
           touchHandle.touch = {};
           touchHandle.touch.reverse = (target != touchHandle);
           me.touchTarget = target;
           touchHandle.touch.startY = touch.pageY;
           touchHandle.touch.lastY = touchHandle.touch.startY;
-          touchHandle.touch.startTime = new Date().getTime()
+          touchHandle.touch.startTime = new Date().getTime();
           touchHandle.touch.lastTime = new Date().getTime();
           touchHandle.touch.distance = null;
           touchHandle.touch.duration = null;
@@ -267,27 +268,35 @@ pui.SlidingScrollBar = function() {
         positionRowNum();
       }
       if (me.designMode) designUtils.preventEvent(event);
-    }
+    };
 
     outerDiv.onmouseup = function() {
       hideRowNum(rowNumHideRequest);
-    }
+    };
     
     outerDiv.appendChild(innerDiv);
     me.container.appendChild(outerDiv); 
     if (touchBar != null) me.container.appendChild(touchBar); 
     if (touchHandle != null) me.container.appendChild(touchHandle); 
     me.container.appendChild(rowNumDiv);    
-  }  
+  };
   
   this.attachOnScroll = function() {
     //outerDiv.onscroll = me.doScroll;
     addEvent(outerDiv, "scroll", me.doScroll);
-  }
+  };
 
   this.doScroll = function(startRowParm) {
     if (!me.ready) return;
-    var startRow = Math.floor(outerDiv.scrollTop / multiplier) + 1;
+    
+    var startRow = Math.floor(outerDiv.scrollTop / multiplier) + 1; //Note: scrollTop isn't reliable in Chrome when zoom isn't 100%.
+    
+    // When setScrollToRow should set the row rather than calculating it from scrollTop; and when doScroll is called with
+    // no arguments or as an event handler, use the targetRow. (i.e. user typed PgUp/PgDn or clicked paging link).
+    if (me.targetRow > 0 && (startRowParm == null || startRowParm.target != null)){
+      startRow = me.targetRow;
+    }
+    
     if (upImg != null && downImg != null && startRowParm == null && prevStartRow > 0) startRowParm = prevStartRow;
     if (startRowParm != null && typeof startRowParm == "number") startRow = startRowParm;
     if (startRow == prevStartRow) return;  // starting row has not changed
@@ -347,10 +356,27 @@ pui.SlidingScrollBar = function() {
         prevStartRow = rowNum;
       }
       var prevTop = outerDiv.scrollTop;
-      outerDiv.scrollTop = (rowNum - 1) * multiplier;
+      outerDiv.scrollTop = (rowNum - 1) * multiplier; //fires "scroll", so doScroll is now in the event queue.
+      me.targetRow = rowNum;   //Give the row number for doScroll to use, putting doScroll in a special state.
       return (prevTop != outerDiv.scrollTop);  // return false if position of scrollbar not changed
     }
-  }
+  };
+  
+  /**
+   * Grid calls this when it finishes loading. Signal that the process that started in setScrollTopToRow is
+   * finished, and doScroll goes back to normal: outerDiv.scrollTop determines the row.
+   * (This helps work around issue 3152, scrollTop is unreliable in Chrome when zoom is not at 100%.)
+   * @returns {undefined}
+   */
+  this.gridRequestFinished = function(){
+    clearTimeout(me.finishTimeout);  //Only the latest timeout needs to run.
+    
+    me.finishTimeout = setTimeout(function(){
+      // This is in a timeout to be cleared after a previously queued onSetRow has run. If this weren't in a timeout,
+      // targetRow would be cleared before a queued second call to doScroll, which could then calculate the wrong row.
+      me.targetRow = null;
+    }, me.interval);
+  };
   
   function sendRow(requestNum, rowNum) {
     if (firstRequest) {
@@ -550,7 +576,7 @@ pui.SlidingScrollBar = function() {
           if (startRow < 1) startRow = 1;
           if (startRow > me.totalRows) startRow = me.totalRows;
           me.doScroll(startRow);
-        }
+        };
         downImg = document.createElement("img");
         downImg.style.position = "absolute";
         downImg.style.bottom = "0px";
@@ -564,18 +590,18 @@ pui.SlidingScrollBar = function() {
           if (startRow < 1) startRow = 1;
           if (startRow > me.totalRows) startRow = me.totalRows;
           me.doScroll(startRow);
-        }
+        };
         outerDiv.appendChild(upImg);
         outerDiv.appendChild(downImg);
       }
     }
     
-  }
+  };
   
   this.increaseHeight = function(y) {
     if (touchBar != null) touchBar.style.height = (parseInt(outerDiv.style.height) + y - 2) + "px";
     else outerDiv.style.height = (parseInt(outerDiv.style.height) + y) + "px";
-  }
+  };
     
   this.hide = function() {
     outerDiv.style.visibility = "hidden";
@@ -586,13 +612,13 @@ pui.SlidingScrollBar = function() {
       touchBar.style.display = "none";    
       touchHandle.style.display = "none";    
     }
-  }
+  };
   
   this.destroy = function() {
     if (outerDiv.parentNode != null) outerDiv.parentNode.removeChild(outerDiv);
     if (innerDiv.parentNode != null) innerDiv.parentNode.removeChild(innerDiv);
     if (rowNumDiv.parentNode != null) rowNumDiv.parentNode.removeChild(rowNumDiv);
-  }
+  };
   
   this.enableMouseWheel = function(gridDom) {
 
@@ -605,6 +631,7 @@ pui.SlidingScrollBar = function() {
     }
     
     /** Event handler for mouse wheel event.
+     * @param {Object} event    Mouse event.
      */
     function wheel(event){
       var delta = 0;
@@ -644,12 +671,12 @@ pui.SlidingScrollBar = function() {
             gridDom.addEventListener('DOMMouseScroll', wheel, false);
     /** IE/Opera. */
     gridDom.onmousewheel = wheel;    
-  }
+  };
 
   this.changeContainer = function(newContainer) {
     me.container = newContainer;
     outerDiv.parentNode.removeChild(outerDiv);
     me.container.appendChild(outerDiv); 
-  }
+  };
 
-}
+};

commit ab4e87aad611ea67a88d4b5124463596656a73b2

Fixed grid crashing when scrollToRow was called with string argument. Issue 3529. (broken since 849235ec)

proddata/js/widgets/grid/Grid.js
@@ -6056,6 +6056,13 @@ pui.Grid = function() {
   };
 
   this["scrollToRow"] = function(row) {
+    row = parseInt(row,10);
+    if (isNaN(row)){
+      //me.recNum cannot be set to a string; otherwise, the grid will crash.
+      //If setScrollTopToRow() passes a string, then the grid will crash after slidingScollBar's doScroll(). #3529.
+      console.log("scrollToRow cannot parseInt:",row);
+      return;
+    }
     if (me.slidingScrollBar) {
       me.scrollbarObj.setScrollTopToRow(row);
     }

proddata/js/widgets/grid/SlidingScrollBar.js
@@ -342,9 +342,9 @@ pui.SlidingScrollBar = function() {
   /**
    * Set the scrollbar position to reflect where rowNum is. This is called when pageUp/pageDown keys 
    * are typed (or paging links are clicked); in grid find; when sorting; and when the grid is setup 
-   * in renderFormat when "subfile record number" is set.
+   * in renderFormat when "subfile record number" is set. Called by grid API, scrollToRow.
    * 
-   * @param {Number} rowNum
+   * @param {Number} rowNum             Assume calling function parsed any strings into integers.
    * @param {Boolean} setPrevStartRow   When true, this.doScroll should return, doing nothing.
    * @returns {Boolean}
    */
@@ -364,7 +364,8 @@ pui.SlidingScrollBar = function() {
         // this prevents onscroll event from processing. Either grid is initialized or an AJAX "find" has responded.
         prevStartRow = rowNum;
       }else{
-        // The grid is handling pageUp, pageDown, or sorting--not being initialized--so set targetRow.
+        // The grid is handling pageUp, pageDown, "scrollToRow", or sorting--not being initialized--so set targetRow.
+        // Note: this cannot be a string, or the grid will crash after doScroll uses targetRow.
         me.targetRow = rowNum;   //Give the row number for doScroll to use, putting doScroll in a special state.
       }
       var prevTop = outerDiv.scrollTop;