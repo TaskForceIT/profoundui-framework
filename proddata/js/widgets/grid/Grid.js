TaskForceIT/profoundui-framework

commit 27b9215600a5918b0d7c66ad6304b329dfa96bad

Add splice(), insertRow(), unshiftRow(), removeRow() methods

proddata/js/widgets/grid/Grid.js
@@ -6877,7 +6877,7 @@ pui.Grid = function () {
     if (refresh) me.refresh();
   }
   
-  this["push"] = function(record, refresh) {
+  function buildEntryFromObject(record) {
     var entry = [];
     for (var i = 0; i < me.fieldNames.length; i++) {
       var fieldName = me.fieldNames[i];
@@ -6885,9 +6885,14 @@ pui.Grid = function () {
       if (!value) value = "";
       entry.push(value);
     }
+    return entry;
+  }
+  
+  this["push"] = function(record, refresh) {    
+    var entry = buildEntryFromObject(record);
     me.dataArray.push(entry);
     if (refresh) me.refresh();
-  }
+  } 
   
   this["addRecords"] = function(records, refresh) {
     for (var i = 0; i < records.length; i++) {
@@ -6902,7 +6907,64 @@ pui.Grid = function () {
     me["addRecords"](records);
     if (refresh) me.refresh();
   }
+  
+  this["splice"] = function(start, deleteCount) {
+    // Adjust start assumming record number (not index) is passed in
+    start = start - 1;    
 
+    // Splice data array
+    var refresh = false;
+    var args = [start, deleteCount];
+    var args2 = [start, deleteCount];
+    for (var i = 2; i < arguments.length; i++) {
+      var arg = arguments[i];
+      if (typeof arg === "boolean" && i + 1 === arguments.length) refresh = true;
+      if (typeof arg !== "object") continue;      
+      var entry = buildEntryFromObject(arg);
+      args.push(entry);
+      args2.push(undefined);
+    }
+    me.dataArray.splice.apply(me.dataArray, args);    
+
+    // Splice domEls
+    for (var i = 0; i < me.runtimeChildren.length; i++) {
+      var domEls = me.runtimeChildren[i].domEls;
+      domEls.splice.apply(domEls, args2);
+    }
+    
+    if (refresh) me.refresh();
+  }
+
+  this["insertRow"] = function(start) {
+    var refresh = false;
+    var args = [start, 0];
+    for (var i = 1; i < arguments.length; i++) {
+      var arg = arguments[i];
+      if (typeof arg === "boolean" && i + 1 === arguments.length) refresh = true;
+      if (typeof arg !== "object") continue;
+      args.push(arg);
+    }
+    me["splice"].apply(me, args);
+  }
+
+  this["unshiftRow"] = function() {
+    var refresh = false;
+    var args = [1, 0];
+    for (var i = 0; i < arguments.length; i++) {
+      var arg = arguments[i];
+      if (typeof arg === "boolean" && i + 1 === arguments.length) refresh = true;
+      if (typeof arg !== "object") continue;
+      args.push(arg);
+    }
+    me["splice"].apply(me, args);
+    if (refresh) me.refresh();
+  }
+
+  this["removeRow"] = function(row, refresh) {
+    me["splice"](row, 1);
+    if (refresh) me.refresh();
+  }
+  
   this["clearState"] = function (part) {
 
     var stg = loadState();

commit d627aba846d48a69ef210ec5ec6f3f8e24aede81

Fixed bug introduced by 27b9215600a5 where grid removeRows was defined twice, causing issues in Designer unobfuscated. (Removing rows was buggy.)

proddata/js/widgets/grid/Grid.js
@@ -291,7 +291,7 @@ pui.Grid = function () {
           var itm = me.tableDiv.designItem;
           itm.designer.undo.start("Remove Grid Row");
           itm.designer.undo.add(itm, "number of rows");
-          me.removeRow();
+          me.removeLastRowCells();
           me.doExpandToLayout();
           me.selectMe();
         }
@@ -543,7 +543,7 @@ pui.Grid = function () {
     if (!me.subfileHidden) {
       me.setProperty("row height", me.rowHeight * me.foldMultiple);
       for (var i = 0; i < rowCount; i++) {
-        me.removeRow();
+        me.removeLastRowCells();
       }
       me.sizeAllCells();
       me.setAllCellStyles();
@@ -2251,7 +2251,7 @@ pui.Grid = function () {
           var tableBottom = parseInt(me.hLines[me.hLines.length - 1].style.top);
           var minRow = me.hasHeader ? 2 : 1;
           if (me.cells.length > minRow) {
-            me.removeRow();
+            me.removeLastRowCells();
             if (tableBottom > parseInt(me.hLines[me.hLines.length - 1].style.top) + me.rowHeight - 3) {
               var addedSome = false;
               while (parseInt(me.hLines[me.hLines.length - 1].style.top) < tableBottom) {
@@ -3720,7 +3720,7 @@ pui.Grid = function () {
           me.mirrorDownAll();
         }
         if (newNumRows < oldNumRows) {
-          while (newNumRows < me.hLines.length - 1) me.removeRow();
+          while (newNumRows < me.hLines.length - 1) me.removeLastRowCells();
         }
         if (me.tableDiv.designItem != null) me.tableDiv.designItem.properties[property] = value;
         me.sendToDesigner();
@@ -5181,7 +5181,7 @@ pui.Grid = function () {
           var tableBottom = parseInt(line.style.top);
           var minRow = me.hasHeader ? 2 : 1;
           if (me.cells.length > minRow) {
-            me.removeRow();
+            me.removeLastRowCells();
             if (tableBottom > parseInt(lines[lines.length - 1].style.top) + me.rowHeight - 3) {
               var addedSome = false;
               while (parseInt(lines[lines.length - 1].style.top) < tableBottom) {
@@ -6755,8 +6755,11 @@ pui.Grid = function () {
     me.setScrollBar();
   };
 
-  this.removeRow = function () {
-    n = me.hLines.length;
+  /**
+   * Remove DOM elements located in the last row, including the DIV and horizontal line element.
+   */
+  this.removeLastRowCells = function () {
+    var n = me.hLines.length;
     if (n <= 0) return;
     n = n - 1;
     me.hLines[n].parentNode.removeChild(me.hLines[n]);