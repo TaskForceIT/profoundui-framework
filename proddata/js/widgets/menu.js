TaskForceIT/profoundui-framework

commit 2548b2097108b0df62e78ae4ba7cc6e65f13dd87

Remove setTimeouts() in menu widget rendering This allows widget set drag/drop proxy to render reliably - 6042

proddata/js/widgets/menu.js
@@ -412,10 +412,6 @@ pui.MenuWidget = function() {
   
   // Public Methods
   this.draw = function() {
-    // Use timeout to avoid unnecessary, repeated drawing when multiple
-    // properties are set.
-    if( me.renderTimeout > 0 ) clearTimeout(me.renderTimeout);
-    me.renderTimeout = setTimeout(function(){
       
       drawMenu({
         container: me.container,
@@ -424,9 +420,6 @@ pui.MenuWidget = function() {
         to: me.choices.length - 1
       });
 
-      me.renderTimeout = null;
-      try{ delete me.renderTimeout;}catch(exc){}
-    }, 0);
   };
   
   this.showSubMenu = function(td) {
@@ -724,6 +717,17 @@ pui.widgets.add({
         }
         break;
     }
+  },
+
+  globalAfterSetter: function(parms) {
+
+    // This prevents the class from being lost in some cases.
+    // The applyPropertyToField() process overwrites dom.className with classes from the properties, so
+    // internally assigned classes can be wiped out.
+    // See Redmine #6042
+    if (parms.propertyName.indexOf("css class") === 0)
+      pui.addCssClass(parms.dom, "pui-menu");
+
   }
   
 });

commit d70bbc8859e2a9f9e95716e4186bf72c1b4251e8

Fix bug introduced in commit 2548b2097108b0d where the menu widget's table element did not get its width set to 100%.  (#6150)

proddata/js/widgets/menu.js
@@ -740,6 +740,10 @@ pui.widgets.add({
     if (parms.propertyName.indexOf("css class") === 0)
       pui.addCssClass(parms.dom, "pui-menu");
 
+    if (parms.propertyName === "width" && parms.dom.menuWidget != null) 
+      parms.dom.menuWidget.draw();
+
+
   }
   
 });